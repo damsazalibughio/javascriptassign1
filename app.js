503580b4c998b616f90128b52dc9149b1c6b2d48
damsazalibughio <81861237+damsazalibughio@users.noreply.github.com>
2021-05-28 10:37:51 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// Task 1
+// alert('Welcome to the amazing website');
+
+// Task 2
+
+//  alert('Error! Please enter a valid password.');
+
+// Task 3
+
+//  alert('Welcome to JS Land...' +'\n'+ 'Happy Coding!');
+
+// Task 4
+
+// alert("welcome to JS Land");
+// alert('Happy Coding!');
+
+// Task 5
+
+// console.log(alert("Hello... I can run JS through my web browser's console"));
+
+// Task 6 & Task 7
+
+// tested a, b, c, d
